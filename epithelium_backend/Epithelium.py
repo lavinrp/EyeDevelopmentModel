d963d42acd617579af4a11c871ce16111d6ef110
Ryan <lavinrp@gmail.com>
2017-09-27 15:06:21 -0400

    i5 - Work on Epithelium.py

---
+++ epithelium_backend/Epithelium.py
@@ -0,0 +1,11 @@
+
+
+from epithelium_backend import Cell
+from epithelium_backend import CellType
+
+
+class Epithelium(object):
+    """A collection of cells that will form an eye"""
+
+    def __init__(self):
+        pass
