8adf4b33f3380d7cbfbb83f38af77fff6d1e219b
Ryan <lavinrp@gmail.com>
2017-09-29 11:48:33 -0400

    added PigmentType, and PhotoreceptorType. Cell now correctly initializes its members.

--- epithelium_backend/Cell.py
+++ epithelium_backend/Cell.py
@@ -1,17 +1,25 @@
+from epithelium_backend.PhotoreceptorType import PhotoreceptorType
+from epithelium_backend.PigmentType import PigmentType
 
 
 class Cell(object):
     """A single cell"""
-    def __init__(self, position=(0, 0, 0), radius=10, cell_types=[]):
+    def __init__(self,
+                 position=(0, 0, 0),
+                 radius=1,
+                 photoreceptor_type=PhotoreceptorType.NOT_RECEPTOR,
+                 pigment_type=PigmentType.NO_PIGMENT):
         """
         Initializes this instance of the Cell class
         :param position: The cartesian coordinates of the cell (x,y,z)
         :param radius: The radius of the cell
-        :param cell_types: The cells specializations
+        :param photoreceptor_type: The cells photoreceptor specialization
+        :param pigment_type: The cells pigmentation specialization
         """
         self.position = position
         self.radius = radius
-        self.cell_types = cell_types
+        self.photoreceptor_type = photoreceptor_type
+        self.pigment_type = pigment_type
 
     def divide(self):
         pass
---
+++ epithelium_backend/PigmentType.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class PigmentType(Enum):
+    """All available cell pigmentation specializations"""
+    NO_PIGMENT = 0
+    PIGMENT = 1
