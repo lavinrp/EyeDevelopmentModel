11e1daa55435ad5cd8e161e13df6a1b863500340
Ryan Lavin <lavinrp@gmail.com>
2017-10-01 15:21:30 -0400

    i3 - Changed cell to store a set of non photoreceptor specializations instead of storing a pigment type

--- epithelium_backend/Cell.py
+++ epithelium_backend/Cell.py
@@ -1,5 +1,5 @@
 from epithelium_backend.PhotoreceptorType import PhotoreceptorType
-from epithelium_backend.PigmentType import PigmentType
+from epithelium_backend.SupportCellType import SupportCellType
 
 
 class Cell(object):
@@ -8,18 +8,19 @@ class Cell(object):
                  position=(0, 0, 0),
                  radius=1,
                  photoreceptor_type=PhotoreceptorType.NOT_RECEPTOR,
-                 pigment_type=PigmentType.NO_PIGMENT):
+                 support_specializations=set()):
         """
         Initializes this instance of the Cell class
-        :param position: The cartesian coordinates of the cell (x,y,z)
-        :param radius: The radius of the cell
-        :param photoreceptor_type: The cells photoreceptor specialization
-        :param pigment_type: The cells pigmentation specialization
+        :param tuple position: The cartesian coordinates of the cell (x,y,z)
+        :param float radius: The radius of the cell
+        :param PhotoreceptorType photoreceptor_type: The cells photoreceptor specialization
+        :param set[SupportCellType] support_specializations: Set of the cells
+         non-photoreceptor specializations
         """
-        self.position = position
-        self.radius = radius
-        self.photoreceptor_type = photoreceptor_type
-        self.pigment_type = pigment_type
+        self.position = position  # type: tuple
+        self.radius = radius  # type: float
+        self.photoreceptor_type = photoreceptor_type  # type: photoreceptor_type
+        self.support_specializations = support_specializations  # type: set[SupportCellType]
 
     def divide(self):
         pass
--- epithelium_backend/PigmentType.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class PigmentType(Enum):
-    """All available cell pigmentation specializations"""
-    NO_PIGMENT = 0
-    PIGMENT = 1
