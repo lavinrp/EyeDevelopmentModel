faba8fe93ba37cebd5c673a02f6016100c0a8220
Ryan Lavin <lavinrp@gmail.com>
2017-10-01 21:07:23 -0400

    i3 - changed type hints to use python 3 style

--- epithelium_backend/Cell.py
+++ epithelium_backend/Cell.py
@@ -5,17 +5,16 @@ from epithelium_backend.SupportCellType import SupportCellType
 class Cell(object):
     """A single cell"""
     def __init__(self,
-                 position=(0, 0, 0),
-                 radius=1,
-                 photoreceptor_type=PhotoreceptorType.NOT_RECEPTOR,
-                 support_specializations=set()):
+                 position: tuple[float, float, float] = (0, 0, 0),
+                 radius: float = 1,
+                 photoreceptor_type: PhotoreceptorType = PhotoreceptorType.NOT_RECEPTOR,
+                 support_specializations: set[SupportCellType] = set()) -> None:
         """
         Initializes this instance of the Cell class
-        :param tuple position: The cartesian coordinates of the cell (x,y,z)
-        :param float radius: The radius of the cell
-        :param PhotoreceptorType photoreceptor_type: The cells photoreceptor specialization
-        :param set[SupportCellType] support_specializations: Set of the cells
-         non-photoreceptor specializations
+        :param: The cartesian coordinates of the cell (x,y,z)
+        :param: The radius of the cell
+        :param: The cells photoreceptor specialization
+        :param: Set of the cells non-photoreceptor specializations
         """
         self.position = position  # type: tuple
         self.radius = radius  # type: float
