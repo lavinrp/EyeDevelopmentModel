fce616658d45b91f2125f62964785856be16a1fd
Ryan Lavin <lavinrp@gmail.com>
2017-10-05 23:30:57 -0400

    HOTFIX - fixed type hints causing a runtime error

--- epithelium_backend/Cell.py
+++ epithelium_backend/Cell.py
@@ -5,10 +5,10 @@ from epithelium_backend.SupportCellType import SupportCellType
 class Cell(object):
     """A single cell"""
     def __init__(self,
-                 position: tuple[float, float, float] = (0, 0, 0),
+                 position: tuple = (0, 0, 0),
                  radius: float = 1,
                  photoreceptor_type: PhotoreceptorType = PhotoreceptorType.NOT_RECEPTOR,
-                 support_specializations: set[SupportCellType] = set()) -> None:
+                 support_specializations: set = set()) -> None:
         """
         Initializes this instance of the Cell class
         :param: The cartesian coordinates of the cell (x,y,z)
@@ -19,7 +19,7 @@ class Cell(object):
         self.position = position  # type: tuple
         self.radius = radius  # type: float
         self.photoreceptor_type = photoreceptor_type  # type: photoreceptor_type
-        self.support_specializations = support_specializations  # type: set[SupportCellType]
+        self.support_specializations = support_specializations  # type: set
 
     def divide(self):
         pass
