0fade41761d114b83fbf7577a0a758673b7a98d8
Ryan <lavinrp@gmail.com>
2017-10-16 16:57:22 -0400

    added 'if main' guard to testing scripts. This will protect them from accidental invocation when running pydoc.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from epithelium_backend.Cell import Cell
 from display_2d.SnapshotDisplay import SnapshotDisplay
 
-cells = []
+if __name__ == '__main__':
 
-# this does weird stuff, but that is fine for the test
-for i in range(1, 100):
-    cells.append(Cell(((i % 10) * 40, (i/10) * 40, 0), 10))
+    cells = []
 
-SnapshotDisplay("test", (500, 500), cells)
-SnapshotDisplay("test2", (500, 500), cells)
+    # this does weird stuff, but that is fine for the test
+    for i in range(1, 100):
+        cells.append(Cell(((i % 10) * 40, (i/10) * 40, 0), 10))
+
+    SnapshotDisplay("test", (500, 500), cells)
+    SnapshotDisplay("test2", (500, 500), cells)
