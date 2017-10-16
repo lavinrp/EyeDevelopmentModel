9ee77b2f918cc2d601a3e60b53f3aee0e6bb12df
bauerbp <bauerbp@mail.uc.edu>
2017-10-16 15:43:40 -0400

    Epithelium with Spring and display

--- epithelium_backend/Cell.py
+++ epithelium_backend/Cell.py
@@ -1,8 +1,10 @@
+from multiprocessing import Lock
+import random
 
 
 class Cell(object):
     """A single cell"""
-    def __init__(self, position=(0, 0, 0), radius=10, cell_types=[]):
+    def __init__(self, position=(0.5, 0.5, 0.5), radius=10.0, cell_types=[]):
         """
         Initializes this instance of the Cell class
         :param position: The cartesian coordinates of the cell (x,y,z)
@@ -12,6 +14,9 @@ class Cell(object):
         self.position = position
         self.radius = radius
         self.cell_types = cell_types
+        self.seed = random.seed()
 
     def divide(self):
-        pass
+        random_position=(random.random(), random.random(), random.random())
+        child_cell = Cell(position=random_position, radius=self.radius)
+        return child_cell
--- epithelium_backend/Epithelium.py
+++ epithelium_backend/Epithelium.py
@@ -1,8 +1,8 @@
 
+# from multiprocessing import Pool, Lock
 
 from epithelium_backend import Cell
-from epithelium_backend import CellType
-
+from epithelium_backend import SpringSimulator
 
 class Epithelium(object):
     """A collection of cells that will form an eye"""
@@ -18,12 +18,42 @@ class Epithelium(object):
 
         self.create_cell_sheet()
 
+    def add_cell(self, cell_from_list):
+        """
+        adds a cell to the list
+        """
+        if len(self.cells) < self.cell_quantity:
+            new_cell = cell_from_list.divide()
+            self.cells.append(new_cell)
+
     def create_cell_sheet(self):
         """
         creates the sheet of cells, populating self.cell
         """
-        self.cells.append(Cell)
+        first_cell = Cell.Cell(position=(0.2, 0.2, 0.2), radius=.2, cell_types=[])
+        second_cell = Cell.Cell(position=(0.3, 0.3, 0.3), radius=.2, cell_types=[])
+        self.cells.append(first_cell)
+        self.cells.append(second_cell)
+        print('We have added the first cell:')
 
-        # should be made parallel
-        while len(self.cells) < self.cell_quantity:
-            self.cells[0].divide()
+        while self.cell_quantity > len(self.cells):
+            for cell in self.cells:
+                self.add_cell(cell)
+
+        print('We have done the mapping')
+
+        print(len(self.cells))
+
+
+if __name__ == '__main__':
+    testEpithelium = Epithelium(20)
+    testEpithelium.create_cell_sheet()
+
+    # Plot the cells as they were spawned
+    SpringSimulator.plot(testEpithelium.cells, 'before.png')
+    # Decompact 250 times with kind of arbitrary parameters
+    SpringSimulator.decompact(testEpithelium.cells, iterations=500000, spring_constant=8, escape=1, dt=1)
+    # Plot the cells after being decompacted
+    SpringSimulator.plot(testEpithelium.cells, 'after.png')
+
+
