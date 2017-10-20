2dfbeefe4c44070e62fdbbd1b13414f0caa69caf
Ryan <lavinrp@gmail.com>
2017-10-20 18:51:52 -0400

    i12 - Added support for the furrow’s event queue.

--- epithelium_backend/Furrow.py
+++ epithelium_backend/Furrow.py
@@ -1,3 +1,6 @@
+import queue
+
+
 class Furrow:
     """
     Simulates a morphogenetic furrow. Stores and maintains all information related to the
@@ -5,15 +8,24 @@ class Furrow:
     events to an Epithelium.
     """
 
-    def __init__(self, position: float = 0, width: float = 1, velocity: float = 0) -> None:
-        """Initialize this instance of Furrow
-        :param position: The horizontal position of this Furrow
-        :param width: The width of the furrows immediate effect
-        :param velocity: how many units the furrow moves in one 'tick'
+    def __init__(self,
+                 position: float = 0,
+                 width: float = 1,
+                 velocity: float = 0,
+                 event_queue: queue.Queue = None) -> None:
+        """Initialize this instance of Furrow.
+        :param position: The horizontal position of this Furrow.
+        :param width: The width of the furrows immediate effect.
+        :param velocity: how many units the furrow moves in one 'tick'.
+        :param event_queue: Events (stored as callable objects) triggered by the progression of the furrow.
         """
         self.position = position  # type: float
         self.width = width  # type: float
         self.velocity = velocity  # type: float
+        self.event_queue = event_queue  # type: queue.Queue
+
+        if self.event_queue is None:
+            self.event_queue = queue.Queue()
 
     def advance(self, distance: float = 0) -> None:
         """
