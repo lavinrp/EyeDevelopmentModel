f9c1639bf0dbb78e6fa9f6e191f1a4a2e109ed00
Ryan Lavin <lavinrp@gmail.com>
2017-10-31 22:35:08 -0400

    i11 - Initial conversion from wx drawing to opengl

--- display_2d/EpitheliumDisplayPanel.py
+++ display_2d/EpitheliumDisplayPanel.py
@@ -1,5 +1,24 @@
 import wx
+from wx import glcanvas
+
+from OpenGL.GL import *
+
 from epithelium_backend.Epithelium import Epithelium
+
+
+class EpitheliumDisplayCanvas(glcanvas.GLCanvas):
+    """OpenGL canvas on which to draw the epithelium"""
+    def __init__(self, parent):
+        # TODO: correctly set the size of EpitheliumDisplayCanvas
+        glcanvas.GLCanvas.__init__(self, parent, size=(10000, 10000), name='epithelium_display_canvas')
+        self.context = glcanvas.GLContext(self)
+        self.SetCurrent(self.context)
+        self.Bind(wx.EVT_PAINT, self.on_draw)
+        glClearColor(0.1, 0.15, 0.1, 1.0)
+
+    def on_draw(self, e):
+        glClear(GL_COLOR_BUFFER_BIT)
+        self.SwapBuffers()
 
 
 class EpitheliumDisplayPanel(wx.Panel):
@@ -9,8 +28,17 @@ class EpitheliumDisplayPanel(wx.Panel):
         # init the panel
         super().__init__(parent, a, b, c, d)
 
+        # create sizer to store gl canvas
+        #self.sizer = wx.FlexGridSizer(cols=1)
+        #self.sizer.AddGrowableCol(0)
+        #self.sizer.AddGrowableRow(0)
+
+        # create gl canvas
+        self.gl_canvas = EpitheliumDisplayCanvas(self)
+        #self.sizer.Add(self.gl_canvas)
+
         # bind events
-        self.Bind(wx.EVT_PAINT, self.on_paint)
+        #self.Bind(wx.EVT_PAINT, self.on_paint)
         self.Bind(wx.EVT_MOUSEWHEEL, self.on_mouse_wheel)
 
         # create default epithelium
@@ -33,7 +61,6 @@ class EpitheliumDisplayPanel(wx.Panel):
         self.zoom += e.GetWheelRotation()/e.GetWheelDelta() * 0.1
         if self.zoom <= min_zoom:
             self.zoom = min_zoom
-
 
     def on_paint(self, e) -> None:
         """
