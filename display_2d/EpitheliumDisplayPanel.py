6e31f0d95eed22f2c23dabca14ed429226786c34
Ryan Lavin <lavinrp@gmail.com>
2017-10-31 21:04:53 -0400

    i11 - added basic zoom logic. Gui does not update as intended

--- display_2d/EpitheliumDisplayPanel.py
+++ display_2d/EpitheliumDisplayPanel.py
@@ -9,10 +9,31 @@ class EpitheliumDisplayPanel(wx.Panel):
         # init the panel
         super().__init__(parent, a, b, c, d)
 
-        # bind the on paint event handler
+        # bind events
         self.Bind(wx.EVT_PAINT, self.on_paint)
+        self.Bind(wx.EVT_MOUSEWHEEL, self.on_mouse_wheel)
 
-        self.epithelium = Epithelium(5)
+        # create default epithelium
+        self.epithelium = Epithelium(5)  # type: Epithelium
+
+        # init drawing vars
+        self.zoom = 1  # type: float
+        self.position_x = 0  # type: float
+        self.position_y = 0  # type: float
+
+    def on_mouse_wheel(self, e: wx.MouseEvent) -> None:
+        """
+        zooms the window based on mouse scrolling
+        :param e: event information
+        :return: None
+        """
+
+        min_zoom = 0.001
+
+        self.zoom += e.GetWheelRotation()/e.GetWheelDelta() * 0.1
+        if self.zoom <= min_zoom:
+            self.zoom = min_zoom
+
 
     def on_paint(self, e) -> None:
         """
@@ -27,4 +48,6 @@ class EpitheliumDisplayPanel(wx.Panel):
         dc.SetPen(wx.Pen(wx.BLACK, 4))
 
         for cell in self.epithelium.cells:
-            dc.DrawCircle(cell.position[0], cell.position[1], cell.radius)
+            dc.DrawCircle((cell.position[0] + self.position_x) * self.zoom,
+                          (cell.position[1] + self.position_y) * self.zoom
+                          , cell.radius * self.zoom)
