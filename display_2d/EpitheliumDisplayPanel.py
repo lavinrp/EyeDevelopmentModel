cedecbfe4827aa6f8ed9db0dec0c13b1cb779173
Ryan Lavin <lavinrp@gmail.com>
2017-12-09 21:15:40 -0500

    i11 - changed the return type of ShaderGenerator.create_program

--- display_2d/EpitheliumDisplayPanel.py
+++ display_2d/EpitheliumDisplayPanel.py
@@ -23,7 +23,6 @@ class EpitheliumDisplayCanvas(glcanvas.GLCanvas):
         self.SwapBuffers()
 
 
-
 class EpitheliumDisplayPanel(wx.Panel):
     """Panel For real-time drawing of an epithelium"""
     def __init__(self, parent, a, b, c, d) -> None:
