4623eb2433c285efa8a5ad0e0a751d7533f1b966
Ryan Lavin <lavinrp@gmail.com>
2018-03-11 23:45:37 -0400

    i46 - Added tests for epithelium_backend.Furrow

--- Tests/TestMain.py
+++ Tests/TestMain.py
@@ -2,6 +2,7 @@
 
 import unittest
 from Tests.epithelium_backend_tests.EpitheliumTester import EpitheliumTester
+from Tests.epithelium_backend_tests.FurrowTester import FurrowTester
 
 if __name__ == '__main__':
     unittest.main()
