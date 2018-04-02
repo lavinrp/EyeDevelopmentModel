2b59cb2b0969ea7194c6724f8d9dcc7ae81ee567
Ryan Lavin <lavinrp@gmail.com>
2018-04-02 15:51:04 -0400

    i52 - unify camera z coord magic numbers

--- display_2d/EpitheliumDisplayCanvas.py
+++ display_2d/EpitheliumDisplayCanvas.py
@@ -36,7 +36,7 @@ class ModernDisplayCanvas(glcanvas.GLCanvas):
                                                                                 self.GetSize().height,
                                                                                 1,
                                                                                 2)
-        self.__view_matrix = matrix44.create_look_at([self.__camera_x, self.__camera_y, -0.9],
+        self.__view_matrix = matrix44.create_look_at([self.__camera_x, self.__camera_y, 0],
                                                      [self.__camera_x, self.__camera_y, -1],
                                                      self.__camera_up_vector)
 
